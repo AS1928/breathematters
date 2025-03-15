2c835101f9017d95ecadfd116cbad8b4c2d53e3a
atoz <132273708+AS1928@users.noreply.github.com>
2025-03-15 10:21:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/vite-deploy/"
+  base: "/breathematters/"
   plugins: [
     react(),
     mode === 'development' &&
